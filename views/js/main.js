06100987bfe88ddffd5996560c513d84f179f39d
Maha-kay <Maha.alkhathlan@gmail.com>
2017-04-29 12:22:00 +0300

    remove FSL

--- views/js/main.js
+++ views/js/main.js
@@ -400,7 +400,6 @@ var pizzaElementGenerator = function(i) {
 
 // resizePizzas(size) is called when the slider in the "Our Pizzas" section of the website moves.
 var resizePizzas = function(size) {
-  debugger;
   window.performance.mark("mark_start_resize");   // User Timing API function
 
   // Changes the value for the size of the pizza above the slider
@@ -450,10 +449,22 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+  	 switch(size) {
+  	 	case "1":
+  	 	 newwidth = 25;
+  	 	 break;
+  	 	case "2":
+  	 	 newwidth = 33.3;
+  	 	 break;
+  	 	case "3":
+  	 	 newwidth = 50;
+  	 	 break;
+  	 	default:
+  	 	 console.log("bug in sizeSwitcher");
+  	 }
+  	var randomPizza = document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < randomPizza.length; i++) {
+      randomPizza[i].style.width = newwidth + "%";
     }
   }
 
@@ -503,8 +514,10 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var scrollTop = document.body.scrollTop / 1250;
+  var phase;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  	phase = Math.sin( scrollTop + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
