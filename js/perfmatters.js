14bd37c7e5f72b4f3d15f38e79ffbd9201c23575
Maha-kay <Maha.alkhathlan@gmail.com>
2017-04-29 07:39:57 +0300

    comment

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -10,7 +10,7 @@ function logCRP() {
 window.addEventListener("load", function(n) {
         logCRP()
     }),
-//Google Analytics
+      // Google Analytics
       (function(w,g){w['GoogleAnalyticsObject']=g;
       w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
       // Optional TODO: replace with your Google Analytics profile ID.
